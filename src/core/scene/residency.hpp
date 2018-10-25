2d8b3b9adf46c265ef9724ce24a0c8125b80a162
Florian Bethe <florian.bethe@tu-clausthal.de>
2018-10-25 11:13:37 +0200

    - Changed device handle structure

--- src/core/scene/residency.hpp
+++ src/core/scene/residency.hpp
@@ -15,101 +15,58 @@ enum class Device : unsigned char {
 	OPENGL	= 4u
 };
 
-
-namespace handle_details {
-
-// General class for specifying device array handle type
-template < Device dev, class T >
-struct DeviceArrayHandleImpl;
-
-template < class T >
-struct DeviceArrayHandleImpl<Device::CPU, T> {
-	using HandleType = std::vector<T>;
-};
-
-template < class T >
-struct DeviceArrayHandleImpl<Device::CUDA, T> {
-	using HandleType = thrust::device_vector<T>;
-};
-
-// TODO: OpenGL handle
-
-} // namespace handle_details
-
-
-/* The following helper structs unify common operations.
- * They are implemented as struct (rather than template functions) to support
- * partial specialization. Often the type T should be kept generic even if
- * the devices are specialized.
- */
-
-// Actual device array handle
-template < Device dev, class T >
-struct DeviceArrayHandle {
+// Generic type for device-something-handles
+template < Device dev, class T, class V >
+struct DeviceHandle {
 	static constexpr Device DEVICE = dev;
 	using Type = T;
-	using HandleType = typename handle_details::DeviceArrayHandleImpl<dev, T>::HandleType;
-
-	HandleType handle;
+	using ValueType = V;
+	using HandleType = ValueType*;
 };
 
-namespace handle_details {
-
-// Helper struct for handle copy
-template < Device dev1, Device dev2, class T >
-struct ArrayCopyer {
-	static void copy(DeviceArrayHandle<dev1, T>& changed, DeviceArrayHandle<dev1, T>& sync) {
-		std::copy(changed.handle.cbegin(), changed.handle.cend(), sync.handle.begin());
-	}
-};
-
-// Helper struct for device array sizes
+// Handle type exclusively for arrays, i.e. they must support vector-esque operations
 template < Device dev, class T >
-struct ArraySize {
-	static std::size_t size(DeviceArrayHandle<dev, T>& sync) {
-		return sync.handle.size();
-	}
+struct DeviceArrayHandle;
 
-	static void resize(DeviceArrayHandle<dev, T>& sync, std::size_t size) {
-		sync.handle.resize(size);
-	}
-};
+template < class T >
+struct DeviceArrayHandle<Device::CPU, T> :
+	public DeviceHandle<Device::CPU, T, std::vector<T>> {};
 
-// TODO: different copy and size behavior for OpenGL!
-
-} // namespace handle_details
-
-// Attribute handles
-template < class T, Device... devs >
-using DeviceArrayHandles = util::TaggedTuple<DeviceArrayHandle<devs, T>...>;
+template < class T >
+struct DeviceArrayHandle<Device::CUDA, T> :
+	public DeviceHandle<Device::CUDA, T, thrust::device_vector<T>> {};
 
 // Operations on the device arrays (override if they differ for some devices)
-template < Device dev, class T >
+template < Device dev, template <Device, class> class H >
 struct DeviceArrayOps {
-	using HandleType = DeviceArrayHandle<dev, T>;
+	template < class T >
+	using HandleType = H<dev, T>;
+	using Type = typename HandleType::Type;
+	using ValueType = typename HandleType::ValueType;
 
-	static std::size_t get_size(const HandleType& sync) {
-		return sync.handle.size();
+	static std::size_t get_size(const ValueType& sync) {
+		return sync.size();
 	}
 
-	static std::size_t get_capacity(const HandleType& sync) {
-		return sync.handle.capacity();
+	static std::size_t get_capacity(const ValueType& sync) {
+		return sync.capacity();
 	}
 	
-	static void resize(HandleType& sync, std::size_t elems) {
-		return sync.handle.resize(elems);
+	static void resize(ValueType& sync, std::size_t elems) {
+		return sync.resize(elems);
 	}
 
-	static void reserve(HandleType& sync, std::size_t elems) {
-		return sync.handle.reserve(elems);
+	static void reserve(ValueType& sync, std::size_t elems) {
+		return sync.reserve(elems);
 	}
 
-	static void clear(HandleType& sync) {
-		sync.handle.clear();
+	static void clear(ValueType& sync) {
+		sync.clear();
 	}
 
 	template < Device other >
-	static void copy(const DeviceArrayHandle<other, T>& changed, HandleType& sync) {
+	static void copy(const typename H<other, T>::ValueType& changed,
+					 ValueType& sync) {
 		// TODO
 		std::runtime_error("Copy between devices not yet supported!");
 		//std::copy(changed.handle.cbegin(), changed.handle.cend(), sync.handle.begin());
