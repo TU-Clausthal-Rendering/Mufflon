19d5f4c720fdf775d88bbcc97aa7266bc07d1b75
Felix <felixbruell@live.de>
2018-10-30 11:04:01 +0100

    gui: added light model for all possible lights

---
+++ project_files/gui/Utility/Vec3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gui.Utility
+{
+    public struct Vec3<T>
+    {
+        public Vec3(T x, T y, T z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public T X { get; }
+        public T Y { get; }
+        public T Z { get; }
+    }
+}
