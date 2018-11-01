7ddf47739bfc6903b1f8ffaf118b4a8e717136dd
Felix <felixbruell@live.de>
2018-11-01 15:41:22 +0100

    gui: added openFileDialog command to envmap light model + added some basic icons

--- project_files/gui/ViewModel/Light/EnvmapLightViewModel.cs
+++ project_files/gui/ViewModel/Light/EnvmapLightViewModel.cs
@@ -5,12 +5,49 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using gui.Model;
 using gui.Model.Light;
 
 namespace gui.ViewModel.Light
 {
     public class EnvmapLightViewModel : LightViewModel
     {
+        private class SelectMapCommand : ICommand
+        {
+            private readonly EnvmapLightModel m_model;
+            private readonly Models m_models;
+
+            public SelectMapCommand(EnvmapLightModel model, Models models)
+            {
+                m_model = model;
+                m_models = models;
+            }
+   
+            public bool CanExecute(object parameter)
+            {
+                return true;
+            }
+
+            public void Execute(object parameter)
+            {
+                var ofd = new Microsoft.Win32.OpenFileDialog
+                {
+                    Multiselect = false,
+                    InitialDirectory = Properties.Settings.Default.ImagePath
+                };
+
+                if (ofd.ShowDialog(m_models.App.Window) != true) return;
+
+                Properties.Settings.Default.ImagePath = System.IO.Path.GetDirectoryName(ofd.FileName);
+                // TODO file name relative?
+                m_model.Map = ofd.FileName;
+            }
+
+            public event EventHandler CanExecuteChanged;
+        }
+
         private readonly EnvmapLightModel m_parent;
 
         public EnvmapLightViewModel(EnvmapLightModel parent) : base(parent)
@@ -38,6 +75,10 @@ namespace gui.ViewModel.Light
         // readonly property
         public string MapFull => m_parent.Map;
 
-        public string MapShort => Path.GetFileName(m_parent.Map);
+        public string MapShort
+        {
+            get => Path.GetFileName(m_parent.Map);
+            set { } // dummy setter because of textbox (requires setter but is disabled)
+        }
     }
 }
