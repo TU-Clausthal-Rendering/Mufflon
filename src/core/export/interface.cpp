TU-Clausthal-Rendering/Mufflon

commit 7b46de937fb507224b7e3eb9aa926e60d673d3b8

core/gui: Fixed wrong screenshot naming

src/core/export/interface.cpp
@@ -2662,7 +2662,7 @@ Boolean render_reset() {
 	CATCH_ALL(false)
 }
 
-Boolean render_save_screenshot(const char* filename) {
+Boolean render_save_screenshot(const char* filename, uint32_t targetIndex, Boolean variance) {
 	TRY
 	auto lock = std::scoped_lock(s_iterationMutex);
 	if(s_currentRenderer == nullptr) {
@@ -2670,38 +2670,53 @@ Boolean render_save_screenshot(const char* filename) {
 		return false;
 	}
 
-	// TODO: this is just for debugging! This should be done by an image library
-	auto dumpPfm = [](std::string fileName, const uint8_t* data) {
-		std::ofstream file(fileName, std::ofstream::binary | std::ofstream::out);
-		if(file.bad()) {
-			logError("[", FUNCTION_NAME, "] Failed to open screenshot file '",
-					 fileName, "'");
-			return;
-		}
-		file.write("PF\n", 3);
-		ei::IVec2 res = s_imageOutput->get_resolution();
-		auto sizes = std::to_string(res.x) + " " + std::to_string(res.y);
-		file.write(sizes.c_str(), sizes.length());
-		file.write("\n-1.000000\n", 11);
+	if(targetIndex >= renderer::OutputValue::TARGET_COUNT) {
+		logError("[", FUNCTION_NAME, "] Invalid target index (", targetIndex, ")");
+		return false;
+	}
 
-		const auto pixels = reinterpret_cast<const char *>(data);
+	fs::path fileName = std::string(filename) + ".pfm";
+	std::ofstream file(fileName, std::ofstream::binary | std::ofstream::out);
+	if(file.bad()) {
+		logError("[", FUNCTION_NAME, "] Failed to open screenshot file '",
+				 fileName.string(), "'");
+		return false;
+	}
+
+	// TODO: this is just for debugging! This should be done by an image library
+
+	const u32 flags = variance ? renderer::OutputValue::make_variance(1u << targetIndex) : (1u << targetIndex);
+	scene::textures::ConstTextureDevHandle_t<Device::CPU> data = s_imageOutput->get_data(renderer::OutputValue{ flags });
+	const int numChannels = textures::NUM_CHANNELS(data->get_format());
+	if(numChannels == 1)
+		file.write("Pf\n", 3);
+	else
+		file.write("PF\n", 3);
+	ei::IVec2 res = s_imageOutput->get_resolution();
+	auto sizes = std::to_string(res.x) + " " + std::to_string(res.y);
+	file.write(sizes.c_str(), sizes.length());
+	file.write("\n-1.000000\n", 11);
+
+	const auto pixels = reinterpret_cast<const char *>(data->data());
+	if(data->get_format() == textures::Format::R32F || data->get_format() == textures::Format::RG32F
+	   || data->get_format() == textures::Format::RGBA32F) {
 		for(int y = 0; y < res.y; ++y) {
 			for(int x = 0; x < res.x; ++x) {
-				file.write(&pixels[(y * res.x + x) * 4u * sizeof(float)], 3u * sizeof(float));
+				switch(numChannels) {
+					case 1: file.write(&pixels[(y * res.x + x) * sizeof(float)], sizeof(float)); break;
+					case 2: {
+						const ei::Vec2& pixel = *reinterpret_cast<ei::Vec2*>(pixels[(y * res.x + x) * 2u * sizeof(float)]);
+						ei::Vec3 tranformed{ pixel };
+						file.write(reinterpret_cast<const char*>(&pixel), 3u * sizeof(float));
+					}	break;
+					default:
+						file.write(&pixels[(y * res.x + x) * numChannels * sizeof(float)], 3u * sizeof(float));
+				}
 			}
 		}
-
-	};
-	const std::string name(filename);
-	for(u32 target : renderer::OutputValue::iterator) {
-		if(s_outputTargets.is_set(target)) {
-			textures::CpuTexture data = s_imageOutput->get_data(renderer::OutputValue{ target }, textures::Format::RGBA32F, false);
-			dumpPfm(name + "_" + std::to_string(target) + ".pfm", data.data());
-		}
-		if(s_outputTargets.is_set(target << 8u)) {
-			textures::CpuTexture data = s_imageOutput->get_data(renderer::OutputValue{ target << 8u }, textures::Format::RGBA32F, false);
-			dumpPfm(name + "_" + std::to_string(target) + "_var.pfm", data.data());
-		}
+	} else {
+		logError("[", FUNCTION_NAME, "] Non-float formats are not supported yet");
+		return false;
 	}
 	logInfo("[", FUNCTION_NAME, "] Saved screenshot '", filename, "'");
 

project_files/gui/Command/ScreenShotCommand.cs
@@ -31,11 +31,20 @@ namespace gui.Command
             filename = filename.Replace("#scenario", m_models.World.CurrentScenario.Name);
             filename = filename.Replace("#renderer", Core.render_get_renderer_name(m_models.Renderer.RendererIndex));
             filename = filename.Replace("#iteration", m_models.Renderer.Iteration.ToString());
-            string targetName = m_models.RenderTargetSelection.VisibleTarget.Name;
-            if (m_models.RenderTargetSelection.IsVarianceVisible)
-                targetName += "(Variance)";
-            filename = filename.Replace("#target", targetName);
-            Core.render_save_screenshot(Path.Combine(m_models.Settings.ScreenshotFolder, filename));
+
+            foreach(RenderTarget target in m_models.RenderTargetSelection.Targets)
+            {
+                if (target.Enabled)
+                {
+                    string file = filename.Replace("#target", target.Name);
+                    Core.render_save_screenshot(Path.Combine(m_models.Settings.ScreenshotFolder, file), target.TargetIndex, 0u);
+                }
+                if(target.VarianceEnabled)
+                {
+                    string file = filename.Replace("#target", target.Name + "(Variance)");
+                    Core.render_save_screenshot(Path.Combine(m_models.Settings.ScreenshotFolder, file), target.TargetIndex, 0u);
+                }
+            }
         }
 
         public event EventHandler CanExecuteChanged

project_files/gui/Dll/Core.cs
@@ -369,8 +369,8 @@ namespace gui.Dll
         [DllImport("core.dll", CallingConvention = CallingConvention.Cdecl)]
         internal static extern bool render_get_screenshot();
         [DllImport("core.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "render_save_screenshot")]
-        private static extern bool render_save_screenshot_(IntPtr filename);
-        internal static bool render_save_screenshot(string filename) { return render_save_screenshot_(StringUtil.ToNativeUtf8(filename)); }
+        private static extern bool render_save_screenshot_(IntPtr filename, UInt32 targetIndex, UInt32 variance);
+        internal static bool render_save_screenshot(string filename, UInt32 targetIndex, UInt32 variance) { return render_save_screenshot_(StringUtil.ToNativeUtf8(filename), targetIndex, variance); }
         [DllImport("core.dll", CallingConvention = CallingConvention.Cdecl)]
         internal static extern UInt32 render_get_render_target_count();
         [DllImport("core.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "render_get_render_target_name")]

src/core/export/interface.h
@@ -506,7 +506,7 @@ CORE_API Boolean CDECL render_iterate();
 CORE_API Boolean CDECL render_reset();
 CORE_API uint32_t CDECL render_get_current_iteration();
 // TODO: what do we pass to the GUI?
-CORE_API Boolean CDECL render_save_screenshot(const char* filename);
+CORE_API Boolean CDECL render_save_screenshot(const char* filename, uint32_t targetIndex, Boolean variance);
 CORE_API uint32_t render_get_render_target_count();
 CORE_API const char* render_get_render_target_name(uint32_t index);
 CORE_API Boolean CDECL render_enable_render_target(uint32_t index, Boolean variance);

commit 7451e892bf5c32f7ce9a497ab294e66e366cc1ce

core: Fixed wrong screenshot normalization (introduced in commit 7b46de93)

src/core/export/interface.cpp
@@ -319,7 +319,6 @@ Boolean core_get_target_image(uint32_t index, Boolean variance,
 			if(format >= TextureFormat::FORMAT_NUM)
 				(void)core_get_target_format(index, &format);
 			s_screenTexture = std::make_unique<textures::CpuTexture>(s_imageOutput->get_data(targetFlags, static_cast<textures::Format>(format), sRgb));
-			textures::ConstTextureDevHandle_t<Device::CPU> texPtr = s_imageOutput->get_data(targetFlags);
 			*ptr = reinterpret_cast<const char*>(s_screenTexture->data());
 		} else {
 			*ptr = nullptr;
@@ -2686,8 +2685,10 @@ Boolean render_save_screenshot(const char* filename, uint32_t targetIndex, Boole
 	// TODO: this is just for debugging! This should be done by an image library
 
 	const u32 flags = variance ? renderer::OutputValue::make_variance(1u << targetIndex) : (1u << targetIndex);
-	scene::textures::ConstTextureDevHandle_t<Device::CPU> data = s_imageOutput->get_data(renderer::OutputValue{ flags });
-	const int numChannels = textures::NUM_CHANNELS(data->get_format());
+	auto data = s_imageOutput->get_data(renderer::OutputValue{ flags },
+										renderer::OutputHandler::get_target_format(renderer::OutputValue{ flags }),
+										false);
+	const int numChannels = textures::NUM_CHANNELS(data.get_format());
 	if(numChannels == 1)
 		file.write("Pf\n", 3);
 	else
@@ -2697,9 +2698,9 @@ Boolean render_save_screenshot(const char* filename, uint32_t targetIndex, Boole
 	file.write(sizes.c_str(), sizes.length());
 	file.write("\n-1.000000\n", 11);
 
-	const auto pixels = reinterpret_cast<const char *>(data->data());
-	if(data->get_format() == textures::Format::R32F || data->get_format() == textures::Format::RG32F
-	   || data->get_format() == textures::Format::RGBA32F) {
+	const auto pixels = reinterpret_cast<const char *>(data.data());
+	if(data.get_format() == textures::Format::R32F || data.get_format() == textures::Format::RG32F
+	   || data.get_format() == textures::Format::RGBA32F) {
 		for(int y = 0; y < res.y; ++y) {
 			for(int x = 0; x < res.x; ++x) {
 				switch(numChannels) {

src/core/renderer/output_handler.cpp
@@ -144,19 +144,6 @@ scene::textures::Format OutputHandler::get_target_format(OutputValue which) {
 	}
 }
 
-scene::textures::ConstTextureDevHandle_t<Device::CPU> OutputHandler::get_data(OutputValue which) {
-	if(!m_targets.is_set(which) || (which.is_variance() && !m_targets.is_set(which >> 8))) {
-		logError("[OutputHandler::get_data] The desired quantity cannot be exported, because it is not recorded!");
-		return nullptr;
-	}
-	const int quantity = which.is_variance() ? ei::ilog2(which >> 8) : ei::ilog2(int(which));
-
-	// Upload to CPU / synchronize if necessary
-	return which.is_variance() ?
-		m_cumulativeVarTex[quantity].acquire_const<Device::CPU>() :
-		m_cumulativeTex[quantity].acquire_const<Device::CPU>();
-}
-
 CpuTexture OutputHandler::get_data(OutputValue which, Format exportFormat, bool exportSRgb) {
 	// Is the current flag, and in case of variance its basic value, set?
 	if(!m_targets.is_set(which) || (which.is_variance() && !m_targets.is_set(which >> 8))) {

src/core/renderer/output_handler.hpp
@@ -164,9 +164,6 @@ public:
 	// exportSRgb: Convert the values from linear to sRGB before packing the data into the exportFormat.
 	scene::textures::CpuTexture get_data(OutputValue which, scene::textures::Format exportFormat, bool exportSRgb);
 
-	// Get the actual texture data (read only) without converting to another format
-	scene::textures::ConstTextureDevHandle_t<Device::CPU> get_data(OutputValue which);
-
 	int get_current_iteration() const noexcept { return m_iteration; }
 
 	int get_width() const { return m_width; }