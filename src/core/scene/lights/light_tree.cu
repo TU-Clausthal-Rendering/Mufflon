882170ddb11835af161605fe117b0510c8497527
Florian Bethe <florian.bethe@tu-clausthal.de>
2019-02-12 09:55:05 +0100

    core: Fixed CUDA light medium kernel

--- src/core/scene/lights/light_tree.cu
+++ src/core/scene/lights/light_tree.cu
@@ -4,31 +4,44 @@
 #include "core/scene/lights/light_medium.hpp"
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
+#include <climits>
 
 namespace mufflon { namespace scene { namespace lights {
 
 __global__ void cuda_set_medium(SceneDescriptor<Device::CUDA> scene, LightSubTree posLights,
-								const u32 NODE_COUNT) {
-	unsigned long long nodeIdx = threadIdx.x + blockDim.x * blockIdx.x;
+								const u32 INTERNAL_NODE_COUNT, const u32 TREE_HEIGHT) {
+	const u32 lightIdx = static_cast<u32>(threadIdx.x + blockDim.x * blockIdx.x);
 
-	if(nodeIdx < NODE_COUNT) {
-		const LightSubTree::Node& node = *posLights.get_node((NODE_COUNT - nodeIdx) * static_cast<u32>(sizeof(LightSubTree::Node)));
-		if(nodeIdx * 2 > posLights.lightCount) {
-			// Single light node;
-			mAssert(node.right.type < static_cast<u16>(LightType::NUM_LIGHTS));
 #ifdef __CUDA_ARCH__
-			set_light_medium(&posLights.memory[node.right.offset], static_cast<LightType>(node.right.type), scene);
-#endif // __CUDA_ARCH__
+	if(lightIdx < posLights.lightCount) {
+		// Special case: only a single light
+		if(posLights.lightCount == 1u) {
+			// Extract information from the root
+			mAssert(posLights.root.type < static_cast<u16>(LightType::NUM_LIGHTS));
+			set_light_medium(posLights.memory, static_cast<LightType>(posLights.root.type), scene);
 		} else {
-			// Dual light node
-			mAssert(node.left.type < static_cast<u16>(LightType::NUM_LIGHTS));
-			mAssert(node.right.type < static_cast<u16>(LightType::NUM_LIGHTS));
-#ifdef __CUDA_ARCH__
-			set_light_medium(&posLights.memory[node.left.offset], static_cast<LightType>(node.left.type), scene);
-			set_light_medium(&posLights.memory[node.right.offset], static_cast<LightType>(node.right.type), scene);
-#endif // __CUDA_ARCH__
+			// Determine what level of the tree the light is on
+			const u32 level = std::numeric_limits<u32>::digits - 1u - static_cast<u32>(__clz(INTERNAL_NODE_COUNT + lightIdx + 1u));
+			// Determine the light's node index within its level
+			const u32 levelIndex = (INTERNAL_NODE_COUNT + lightIdx) - ((1u << level) - 1u);
+			// The corresponding parent node's level index is then the level index / 2
+			const u32 parentLevelIndex = levelIndex / 2u;
+			// Finally, compute the tree index of the node
+			const u32 parentIndex = (1u << (level - 1u)) - 1u + parentLevelIndex;
+			const LightSubTree::Node& node = *posLights.get_node(parentIndex * sizeof(LightSubTree::Node));
+
+			// Left vs. right node
+			// TODO: for better divergence let all even indices be processes first, then the uneven ones
+			if(levelIndex % 2 == 0) {
+				mAssert(node.left.type < static_cast<u16>(LightType::NUM_LIGHTS));
+				set_light_medium(&posLights.memory[node.left.offset], static_cast<LightType>(node.left.type), scene);
+			} else {
+				mAssert(node.right.type < static_cast<u16>(LightType::NUM_LIGHTS));
+				set_light_medium(&posLights.memory[node.right.offset], static_cast<LightType>(node.right.type), scene);
+			}
 		}
 	}
+#endif // __CUDA_ARCH__
 }
 
 namespace lighttree_detail {
@@ -38,12 +51,13 @@ void update_media_cuda(const SceneDescriptor<Device::CUDA>& scene, const LightSu
 
 	int blockSize = 256;
 	int gridSize;
-	if(NODE_COUNT == 0)
+	if(posLights.lightCount == 0)
 		gridSize = 1;
 	else
-		gridSize = (NODE_COUNT - 1) / blockSize + 1;
+		gridSize = (static_cast<int>(posLights.lightCount) - 1) / blockSize + 1;
 
-	cuda_set_medium<<<gridSize, blockSize>>>(scene, posLights, NODE_COUNT);
+	cuda_set_medium<<<gridSize, blockSize>>>(scene, posLights, NODE_COUNT,
+											 static_cast<u32>(std::log2(posLights.lightCount)));
 	cuda::check_error(cudaGetLastError());
 }
 
