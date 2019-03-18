c20a21250b0943c9cce1ab2111d2977b858a80e5
Johannes Jendersie <jojendersie@web.de>
2019-03-18 08:28:09 +0100

    added missing file: parallel.cpp

--- src/util/parallel.cpp
+++ src/util/parallel.cpp
@@ -4,11 +4,24 @@
 namespace mufflon {
 
 int get_thread_num() {
-	return ::omp_get_num_threads();
+#ifndef DEBUG_ENABLED
+	// omp_get_thread_num() always returns 1 in sequential code.
+	// https://stackoverflow.com/a/13328691/1913512
+	int n = 0;
+	#pragma omp parallel reduction(+:n)
+	n += 1;
+	return n;
+#else
+	return 1;
+#endif
 }
 
 int get_current_thread_idx() {
-	return ::omp_get_thread_num();
+#ifndef DEBUG_ENABLED
+	return omp_get_thread_num();
+#else
+	return 0;
+#endif
 }
 
 } // namespace mufflon
