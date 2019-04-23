51b15bd1c047eefa1b4e82e39fbc295d11c03681
Florian Bethe <florian.bethe@tu-clausthal.de>
2019-04-23 12:35:54 +0200

    pythonbinding: Updated example

--- src/pythonbinding/example.py
+++ src/pythonbinding/example.py
@@ -1,18 +1,24 @@
 import bindings
+from bindings import LogLevel
+from bindings import Device
 
 
 def render():
     renderer = bindings.RenderActions()
-    if not renderer.enable_renderer("Pathtracer"):
-        print("Could not find specified renderer")
-        return
-    if renderer.load_json(sceneJson="Path/To/Scene.json") != 0:  # 0 == Loader Succeded
-        return
-    if not renderer.load_scenario("TestScenario"):
-        print("Could not find specified scenario")
-        return
+    renderer.set_renderer_log_level(LogLevel.PEDANTIC)
+    renderer.set_loader_log_level(LogLevel.WARNING)
+    
+    # Name is case insensitive
+    renderer.enable_renderer("PT", [ Device.CUDA ])
+    #renderer.enable_renderer("pt", [ Device.CUDA ])
+    #renderer.enable_renderer("patHtraCer", [ Device.CPU ])
+    
+    
+    renderer.load_json(sceneJson="pathToScene.json")
+    renderer.load_scenario("ScenarioName")
 
     renderer.render_for_seconds(10)
-    renderer.render_for_iterations(10)
+    # Printing defaults to False
+    renderer.render_for_iterations(16, printProgress=True, progressSteps=8)
 
 render()
