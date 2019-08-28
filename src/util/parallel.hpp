28315c43a8e9e8981c865ce4f5d728caff0bdc8a
Florian Bethe <florian.bethe@tu-clausthal.de>
2019-08-28 11:12:02 +0200

    core: Added scheduling strategy with minimum chunk size to PARALLEL_FOR

--- src/util/parallel.hpp
+++ src/util/parallel.hpp
@@ -3,8 +3,10 @@
 #include "util/assert.hpp"
 
 #ifndef DEBUG_ENABLED
-#define PARALLEL_FOR omp parallel for
-#define PARALLEL_REDUCTION(op, var) omp parallel for reduction (op:var)
+// The chunk size and scheduling type is an "best guess";
+// the default values are abysmal for our case
+#define PARALLEL_FOR omp parallel for schedule(static, 1000)
+#define PARALLEL_REDUCTION(op, var) omp parallel for schedule(static, 1000) reduction (op:var)
 #else // DEBUG_ENABLED
 #define PARALLEL_FOR
 #endif // DEBUG_ENABLED
