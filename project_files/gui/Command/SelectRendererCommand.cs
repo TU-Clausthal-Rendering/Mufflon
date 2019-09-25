e967f11396e44465765397253dee8c6967e6a9ba
Florian Bethe <florian.bethe@tu-clausthal.de>
2019-09-25 11:07:59 +0200

    gui: Fixed exception when changing from CUDA to CPU renderer of different type (resolving #176)

--- project_files/gui/Command/SelectRendererCommand.cs
+++ project_files/gui/Command/SelectRendererCommand.cs
@@ -25,7 +25,7 @@ namespace gui.Command
             var data = new SelectRendererViewModel();
             var dialog = new SelectRendererDialog(data);
             if (dialog.ShowDialog() != true) return;
-            m_models.Renderer.RendererIndex = data.TypeValue;
+            m_models.Renderer.SetRenderer(data.TypeValue, 0);
             // TODO: display the current renderer somewhere
         }
 
