48fffd083432ef9e68a4c422001382ff2f6ac4e5
Florian Bethe <florian.bethe@tu-clausthal.de>
2019-09-25 15:03:55 +0200

    core: Fixed wrong element initialization in CUDA device pointers

--- src/core/memory/allocator.cu
+++ src/core/memory/allocator.cu
@@ -1,21 +1,24 @@
 #include "allocator.hpp"
 #include "synchronize.hpp"
+#include <device_launch_parameters.h>
 
 
 namespace mufflon {
 namespace memory_details {
 
-__global__ void cuda_copy_element(const void* devElemMem, void* targetMem, const std::size_t elemBytes,
+__global__ void cuda_copy_element(const void* srcMem, void* dstMem, const std::size_t elemBytes,
 							 const std::size_t count) {
+	if(threadIdx.x == 0 && threadIdx.y == 0)
 	for(std::size_t i = 0u; i < count; ++i)
-		memcpy(targetMem, devElemMem, elemBytes);
+		memcpy(dstMem, srcMem, elemBytes);
 }
 
+// Element is the (host-side) source, targetMem the (device-side) destination
 void copy_element(const void* element, void* targetMem, const std::size_t elemBytes,
 				  const std::size_t count) {
 	void* deviceMem;
 	cuda::check_error(cudaMalloc(&deviceMem, elemBytes));
-	cudaMemcpy(deviceMem, targetMem, elemBytes, cudaMemcpyDefault);
+	cudaMemcpy(deviceMem, element, elemBytes, cudaMemcpyDefault);
 
 	cuda_copy_element<<< 1, 1024 >>>(deviceMem, targetMem, elemBytes, count);
 
