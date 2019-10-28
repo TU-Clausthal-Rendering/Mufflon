8c41ad7d898bd8be72de9c4d9711ef21e41868c2
Florian Bethe <florian.bethe@tu-clausthal.de>
2019-10-28 15:14:28 +0100

    core: Focus camera is now unbiased; Removed unnecessary parameters

--- src/core/cameras/focus.hpp
+++ src/core/cameras/focus.hpp
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include "camera.hpp"
 #include "core/export/api.h"
@@ -21,8 +21,6 @@ public:
 		  float sensorHeight, float near = 1e-4f,
 		  float far = 1e10f) :
 		Camera(CameraModel::FOCUS, position, dir, up, pathCount, near, far),
-		m_vFov(2.f * std::atan(sensorHeight / (2.f * focalLength))),
-		m_tanVFov(std::tan(m_vFov / 2.f)),
 		m_focalLength(focalLength),
 		m_focusDistance(focusDist),
 		m_lensRadius(lensRad),
@@ -34,7 +32,6 @@ public:
 	float get_lens_radius() const noexcept { return m_lensRadius; }
 	float get_aperture_in_f_stops() const noexcept { return m_focalLength / (2.f * m_lensRadius); }
 	float get_sensor_height() const noexcept { return m_sensorHeight; }
-	float get_vertical_fov() const noexcept { return m_vFov; }
 	void set_focus_distance(float distance) noexcept {
 		m_focusDistance = distance;
 		m_isDirty = true;
@@ -62,8 +59,6 @@ public:
 	// Get the required size of a parameter bundle.
 	std::size_t get_parameter_pack_size() const final;
 private:
-	Radians m_vFov;			// Vertical field of view in radiant.
-	float m_tanVFov;		// Tangents of the vfov halfed
 	float m_focalLength;	// Focal length in meter
 	float m_focusDistance;	// Focus distance in meter
 	float m_lensRadius;		// Lens radius in meter
@@ -74,12 +69,11 @@ private:
 // TODO: smaller size and better alignment by packing one of the directions?
 struct FocusParams : public CameraParams {
 	scene::Point position;
-	float tanVFov;
+	float sensorDistance;
 	scene::Direction viewDir;
 	float far;
 	scene::Direction up;
-	__half sensorHalfHeight;
-	__half sensorDistance;
+	float sensorHalfHeight;
 	float focusDistance;		// How far away is a sharp point from the lens
 	float focalLength;			// Focal length of the lens
 	float lensRadius;
@@ -90,28 +84,27 @@ CUDA_FUNCTION math::PositionSample
 focuscam_sample_position(const FocusParams& params, const math::RndSet2& rndSet) {
 	// First sample a point on a unit disk; for that, we project the unit square onto it (see PBRT)
 	// TODO: is this cheaper than sqrt(r) * sin/cos(theta)?
-	// Get a (randomized) position in [-1,1]�
+	// Get a (randomized) position in [-1,1]²
 	const ei::Vec2 squarePoint{ 2.f * rndSet.u0 - 1.f, 2.f * rndSet.u1 - 1.f };
 	float theta, r;
 	if(ei::abs(squarePoint.x) > ei::abs(squarePoint.y)) {
 		r = squarePoint.x;
 		theta = ei::PI * squarePoint.y / (4.f * squarePoint.x);
-	}
-	else {
+	} else {
 		r = squarePoint.y;
 		theta = ei::PI / 2.f - ei::PI * squarePoint.x / (4.f * squarePoint.y);
 	}
 	// Adjust the sample to be within the lens radius
 	const ei::Vec2 diskSample = params.lensRadius * r * ei::Vec2{ std::cos(theta), std::sin(theta) };
 	// Transform the sample to camera space
-	const ei::Vec3 nPos{ diskSample.x, diskSample.y, __half2float(params.sensorDistance) };
+	const ei::Vec3 nPos{ diskSample.x, diskSample.y, params.sensorDistance };
 	// Go to world space
 	const ei::Vec3 xAxis = ei::cross(params.viewDir, params.up);
 	const ei::Vec3 yAxis = ei::cross(params.viewDir, xAxis);
 	const ei::Vec3 dirWorld = xAxis * nPos.x + yAxis * nPos.y + params.viewDir * nPos.z;
 	const ei::Vec3 lensPos = params.position + dirWorld;
 	// Compute the PDF for sampling the given position on the lens
-	const AreaPdf lensPosPdf = AreaPdf{ 1.f / ei::PI };
+	const AreaPdf lensPosPdf = AreaPdf{ 1.f / (ei::PI * params.lensRadius * params.lensRadius) };
 
 	return math::PositionSample{ lensPos, lensPosPdf };
 }
@@ -120,74 +113,106 @@ CUDA_FUNCTION Importon
 focuscam_sample_ray(const FocusParams& params, const scene::Point& exitPosWorld,
 					const Pixel& pixel, const math::RndSet2& rndSet) {
 	// First we sample the sensor position, then we use the sensor and the lens position to compute the direction
-	// Get a (randomized) position in [-1,1]�
+	// Get a (randomized) position in [-1,1]²
 	const ei::Vec2 subPixel = pixel + ei::Vec2(rndSet.u0, rndSet.u1);
 	const ei::Vec2 canonicalPos = 2.f * subPixel / params.resolution - 1.0f;
-	const float sensorHalfHeight = __half2float(params.sensorHalfHeight);
+	const float sensorHalfHeight = params.sensorHalfHeight;
 	const float aspectRatio = params.resolution.x / float(params.resolution.y);
 	// Scale to fit the sensor
-	const ei::Vec2 nPos = sensorHalfHeight * ei::Vec2{canonicalPos.x * aspectRatio, canonicalPos.y};
+	const ei::Vec2 nPos = sensorHalfHeight * ei::Vec2{ canonicalPos.x * aspectRatio, canonicalPos.y };
 	// Go to world space
 	const ei::Vec3 xAxis = ei::cross(params.viewDir, params.up);
 	const ei::Vec3 yAxis = ei::cross(params.viewDir, xAxis);
-	const ei::Vec3 sensorPos = params.position + xAxis * nPos.x + yAxis * nPos.y;
+	const ei::Vec3 sensorPos = params.position - xAxis * nPos.x + yAxis * nPos.y;
 
 	// To compute the ray direction, we compute the intersection
 	// point between an unperturbed ray through the lens centre
 	// and the focus plane
-	const ei::Vec3 lensCentre = params.position + params.viewDir * __half2float(params.sensorDistance);
-	const ei::Vec3 sensorToLensCentre = ei::normalize(lensCentre - sensorPos);
+	const ei::Vec3 lensCentre = params.position + params.viewDir * params.sensorDistance;
+	const ei::Vec3 sensorToLensCentre = lensCentre - sensorPos;
 	const ei::Vec3 focalPlane = params.position + params.viewDir * params.focusDistance;
-	const float t = ei::dot(focalPlane - sensorPos, params.viewDir) / ei::dot(sensorToLensCentre, params.viewDir);
+	const float sDotV = ei::dot(sensorToLensCentre, params.viewDir);
+	mAssert(sDotV != 0.f);
+	const float t = ei::dot(focalPlane - sensorPos, params.viewDir) / sDotV;
 	const auto focalPoint = sensorPos + sensorToLensCentre * t;
 	const auto direction = ei::normalize(focalPoint - exitPosWorld);
+	const float cosTheta = ei::dot(direction, params.viewDir);
 
-	// Compute the directional PDF (see PBRT)
-	const float pixelArea = ei::sq(2.f * params.tanVFov) * aspectRatio;
-	const float cosPixelDir = ei::dot(direction, params.viewDir);
-	const AngularPdf pixelPdf{ 1.f / (pixelArea * cosPixelDir * cosPixelDir * cosPixelDir) };
+	// How and why the PDF is like this: see projection code (below)
+	const float sensorArea = 4.f * sensorHalfHeight * sensorHalfHeight / aspectRatio;
+	const float pdf = params.sensorDistance * params.sensorDistance / (sensorArea * cosTheta * cosTheta * cosTheta);
 
 	return Importon{
-		math::DirectionSample{ direction, pixelPdf },
-		static_cast<float>(pixelPdf) // TODO: is this correct?
+		math::DirectionSample{ direction, AngularPdf{ pdf } },
+		pdf
 	};
 }
 
 // Compute pixel position and PDF
 // position: a direction in world space.
 CUDA_FUNCTION ProjectionResult
-focuscam_project(const FocusParams& params, const scene::Point& lensPoint, const scene::Direction& excident) {
-	// Project the ray back onto the sensor
-	// Taken from PBRT
+focuscam_project(const FocusParams& params, const scene::Point& lensPoint,
+				 const scene::Direction& excident) {
 	const float cosTheta = ei::dot(excident, params.viewDir);
-	const float focusDistance = params.focusDistance / cosTheta;
-	const ei::Vec3 focusPoint = lensPoint - focusDistance * excident;
-	const ei::Vec3 sensorPoint = focusPoint - (__half2float(params.sensorDistance) - focusDistance) * params.viewDir;
+	const float sensorDist = params.sensorDistance;
+	// Early-reject if we try to project from the wrong side
+	if(cosTheta <= 0.f)
+		return ProjectionResult{};
+
+	// Compute the focus point; this is the intersection with the focus plane at focusDistance
+	// Since we have the angle with the view direction this is a simple division
+	const ei::Vec3 focalPoint = lensPoint + excident * (params.focusDistance / cosTheta);
+	// With this we can compute the lens-centre ray
+	const ei::Vec3 lensCentre = params.position + params.viewDir * sensorDist;
+	// Scale the direction with the sensor distance to improve FP precision
+	const ei::Vec3 focalToLensCentre = sensorDist * (lensCentre - focalPoint);
+	// The intersection with the sensor plane gives us the sensor point
+	const float fDotV = ei::dot(focalToLensCentre, params.viewDir);
+	if(fDotV == 0.f)
+		return ProjectionResult{};
+	const float t = ei::dot(params.position - lensCentre, params.viewDir) / fDotV;
+	const ei::Vec3 sensorPoint = lensCentre + t * focalToLensCentre;
 
 	// Map to UVs
 	const ei::Vec3 xAxis = ei::cross(params.viewDir, params.up);
 	const ei::Vec3 yAxis = ei::cross(params.viewDir, xAxis);
 	const ei::Vec3 sensorDir = sensorPoint - params.position;
+	// Flip the Y coordinate to flip the image properly
 	ei::Vec2 uv{ ei::dot(sensorDir, xAxis), ei::dot(sensorDir, yAxis) };
-
 	// Compute screen coordinate for this position
-	const float sensorHalfHeight = __half2float(params.sensorHalfHeight);
-	float aspectRatio = params.resolution.x / float(params.resolution.y);
-	uv.x *= 1.f / aspectRatio;
-	uv *= sensorHalfHeight;
+	const float sensorHalfHeight = params.sensorHalfHeight;
+	uv /= sensorHalfHeight;		// Bring UV into [-a,a]x[-1,1]
+	const float aspectRatio = params.resolution.y / float(params.resolution.x);
+	uv.x *= -aspectRatio;		// Bring UV into [-1,1]x[-1,1]
 
 	// On screen?
 	if(!(uv.x > -1 && uv.x <= 1 && uv.y > -1 && uv.y <= 1))
 		return ProjectionResult{};
 
-	Pixel pixelCoord{ floor((uv * -0.5f + 0.5f) * params.resolution) };
+	Pixel pixelCoord{ floor((uv * 0.5f + 0.5f) * params.resolution) };
 	// Need to check the boundaries. In rare cases values like uv.x==-0.999999940
 	// cause pixel coordinates in equal to the resolution.
 	if(pixelCoord.x >= params.resolution.x) { pixelCoord.x = u32(params.resolution.x) - 1; }
 	if(pixelCoord.y >= params.resolution.y) { pixelCoord.y = u32(params.resolution.y) - 1; }
 
-	float pixelArea = ei::sq(2.0f * params.tanVFov) * aspectRatio;
-	float pdf = 1.0f / (pixelArea * cosTheta * cosTheta * cosTheta);
+	/* The thin-lens model has two sampling events: the origin is determined on the (virtual)
+	 * lens and has the uniform area PDF p(A_L) = 1/(π * r²).
+	 * The second sampling is slightly more involved: we start by sampling in a given pixel
+	 * with the PDF p(A_S) = 1 / A_Sensor = RES_X * RES_Y / (W * H).
+	 * To transform this to a directional PDF, we us the solid angle conversion:
+	 * p(ω) = RES_X * RES_Y * d² / (W * H * cos(Θ) * cos³(θ)), where d is the distance
+	 * between lens and sensor centre, and θ is the angle between view direction and
+	 * OUTGOING direction, ie. the vector from the lens sample point into the scene.
+	 * The lone cos(θ) comes from the angle conversion, the cos³(θ) comes from the
+	 * domain transition and the associated Jacobian.
+	 * Now, the lone cos(θ) gets cancelled out when you take into account irradiance
+	 * transfer, which falls off with exactly the same cosine. Additionally, the fact that
+	 * we sample for one pixel is already compensated for in our renderers and is thus not
+	 * part of the result either. Finally, the camera response function is equal to the PDF
+	 * for us.
+	 */
+	const float sensorArea = 4.f * sensorHalfHeight * sensorHalfHeight / aspectRatio;
+	const float pdf = params.sensorDistance * params.sensorDistance / (sensorArea * cosTheta * cosTheta * cosTheta);
 
 	return ProjectionResult{
 		pixelCoord,
